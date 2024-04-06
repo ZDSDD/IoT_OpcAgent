336d89611b8466ac97f0ec966f5736a0556a97c3
ZDSDD <106777224+ZDSDD@users.noreply.github.com>
2024-04-06 22:38:54 +0200

    Introduce ProductionLineManager for communication with OPC UA server

---
+++ OpcAgent.Lib/ProductionLineManager.cs
@@ -0,0 +1,41 @@
+﻿using Opc.Ua;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+
+namespace OpcAgent.Lib;
+
+public class ProductionLineManager
+{
+    private readonly OpcClient _client;
+    private readonly NodeId _nodeId;
+
+    private readonly Dictionary<OpcEndpoint, OpcReadNode> _readValuesCommands;
+    private readonly Dictionary<OpcEndpoint, OpcReadNode> _readAttributeCommands;
+
+    public ProductionLineManager(OpcClient client, NodeId nodeId)
+    {
+        _client = client;
+        _nodeId = nodeId;
+        _readValuesCommands = OpcUtils.InitReadNodes(this._nodeId);
+        _readAttributeCommands = OpcUtils.InitReadNameNodes(this._nodeId);
+    }
+
+    public void EmergencyStop()
+    {
+        object[] emergencyStopResult = _client.CallMethod(
+            _nodeId,
+            $"{_nodeId}/{OpcEndpoint.EmergencyStop}");
+    }
+    
+    public void LogAllInfo()
+    {
+        IEnumerable<OpcValue> job = _client.ReadNodes(_readValuesCommands.Values.ToArray());
+        IEnumerable<OpcValue> jobName = _client.ReadNodes(_readAttributeCommands.Values.ToArray());
+        var valuesAndNames = job.Zip(jobName, (first, second) => second + ": " + first);
+        Console.WriteLine();
+        foreach (var item in valuesAndNames)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}

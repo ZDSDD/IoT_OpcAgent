cfac1436443366064fc8bf9faa51186334ffb299
ZDSDD <106777224+ZDSDD@users.noreply.github.com>
2024-04-06 22:10:42 +0200

    Introduce OpcUtils class for generating dictionaries of OPC read nodes

---
+++ OpcAgent.Lib/OpcUtils.cs
@@ -0,0 +1,35 @@
+﻿using Opc.Ua;
+using Opc.UaFx;
+
+namespace OpcAgent.Lib;
+
+public static class OpcUtils
+{
+    internal static Dictionary<OpcEndpoint, OpcReadNode> InitReadNodes(NodeId nodeId)
+    {
+        return new Dictionary<OpcEndpoint, OpcReadNode>
+        {
+            {OpcEndpoint.ProductionStatus, new OpcReadNode($"{nodeId}/{OpcEndpoint.ProductionStatus}") },
+            {OpcEndpoint.ProductionRate, new OpcReadNode($"{nodeId}/{OpcEndpoint.ProductionRate}") },
+            {OpcEndpoint.WorkorderId, new OpcReadNode($"{nodeId}/{OpcEndpoint.WorkorderId}") },
+            {OpcEndpoint.Temperature, new OpcReadNode($"{nodeId}/{OpcEndpoint.Temperature}") },
+            {OpcEndpoint.GoodCount, new OpcReadNode($"{nodeId}/{OpcEndpoint.GoodCount}") },
+            {OpcEndpoint.BadCount, new OpcReadNode($"{nodeId}/{OpcEndpoint.BadCount}") },
+            {OpcEndpoint.DeviceError, new OpcReadNode($"{nodeId}/{OpcEndpoint.DeviceError}") }
+        };
+    }
+
+    internal static Dictionary<OpcEndpoint, OpcReadNode> InitReadNameNodes(NodeId nodeId)
+    {
+        return new Dictionary<OpcEndpoint, OpcReadNode>
+        {
+            {OpcEndpoint.ProductionStatus, new OpcReadNode($"{nodeId}/{OpcEndpoint.ProductionStatus}", OpcAttribute.DisplayName)},
+            {OpcEndpoint.ProductionRate, new OpcReadNode($"{nodeId}/{OpcEndpoint.ProductionRate}", OpcAttribute.DisplayName)},
+            {OpcEndpoint.WorkorderId, new OpcReadNode($"{nodeId}/{OpcEndpoint.WorkorderId}", OpcAttribute.DisplayName)},
+            {OpcEndpoint.Temperature, new OpcReadNode($"{nodeId}/{OpcEndpoint.Temperature}", OpcAttribute.DisplayName)},
+            {OpcEndpoint.GoodCount, new OpcReadNode($"{nodeId}/{OpcEndpoint.GoodCount}", OpcAttribute.DisplayName)},
+            {OpcEndpoint.BadCount, new OpcReadNode($"{nodeId}/{OpcEndpoint.BadCount}", OpcAttribute.DisplayName)},
+            {OpcEndpoint.DeviceError, new OpcReadNode($"{nodeId}/{OpcEndpoint.DeviceError}", OpcAttribute.DisplayName)}
+        };
+    }
+}

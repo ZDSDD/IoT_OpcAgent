5b7d97e808d438c129a334f3be0c3bcdbbc97642
ZDSDD <106777224+ZDSDD@users.noreply.github.com>
2024-04-06 22:05:53 +0200

    Implement Selector Base for ProductionLineFeatureSelector and IoTHubFeatureSelector

---
+++ OpcAgent/Selector/Implementation/ProductionLineFeatureSelector.cs
@@ -0,0 +1,29 @@
+﻿using OpcAgent.Enums.Feature;
+using OpcAgent.Lib;
+
+namespace OpcAgent.Selector.Implementation;
+
+public class ProductionLineFeatureSelector(ProductionLineManager manager) : SelectorBase
+{
+    public override void PrintMenu()
+    {
+        Console.WriteLine(@"
+            1 - Log all info from device
+            2 - Emergency stop");
+    }
+
+    public void Execute(ProductionLineFeature feature)
+    {
+        switch (feature)
+        {
+            case ProductionLineFeature.LogAll:
+                manager.LogAllInfo();
+                break;
+            case ProductionLineFeature.EmergencyStop:
+                manager.EmergencyStop();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(feature), feature, null);
+        }
+    }
+}

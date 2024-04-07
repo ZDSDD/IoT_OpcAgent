a5b1406109378228b60cb41cd3dda08427884ed1
ZDSDD <106777224+ZDSDD@users.noreply.github.com>
2024-04-07 14:59:04 +0200

    Clean up code, add comments

--- OpcAgent/Program.cs
+++ OpcAgent/Program.cs
@@ -1,29 +1,47 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Configuration;
 using Opc.Ua;
 using Opc.UaFx.Client;
 using OpcAgent;
 using OpcAgent.Enums.Feature;
 using OpcAgent.Lib;
+using OpcAgent.Lib.Device;
 using OpcAgent.Lib.Selector.Implementation;
+using TransportType = Microsoft.Azure.Devices.Client.TransportType;
 
-
+//Get keys from secrets.json
 IConfigurationRoot config = new ConfigurationBuilder()
     .AddUserSecrets<Program>()
     .Build();
+
+//Initialize Device Client
+string deviceConnectionString = config.GetConnectionString("Device1");
+await using var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+await deviceClient.OpenAsync();
+
+//Initialize Virtual Device
+var device = new VirtualDevice(deviceClient);
+
+//Initialize OPCClient
 string serverAddress = config.GetConnectionString("serverAddress");
-// string serviceConnectionString = config["IoTHub"];
-// using var serviceClient = ServiceClient.CreateFromConnectionString(serviceConnectionString);
-// using var registryManager = RegistryManager.CreateFromConnectionString(serviceConnectionString);
-
-
 using var client = new OpcClient(serverAddress);
-
 client.Connect();
+
+//Initialize Production Line Manager
 NodeId nodeId = new NodeId("ns=2;s=Device 1");
+ProductionLineManager productionLineManager = new ProductionLineManager(client, nodeId, device);
 
-ProductionLineManager productionLineManager = new ProductionLineManager(client, nodeId);
+//Initialize IoT Hub Manager
+string serviceConnectionString = config["IoTHub"];
+using var serviceClient = ServiceClient.CreateFromConnectionString(serviceConnectionString);
+using var registryManager = RegistryManager.CreateFromConnectionString(serviceConnectionString);
+var iotHubManager = new IoTHubManager(serviceClient, registryManager);
 
+//Initialize Selectors
 ProductionLineFeatureSelector selector = new ProductionLineFeatureSelector(productionLineManager);
+
+//Main menu
 int input;
 do
 {
@@ -32,4 +50,7 @@ do
     selector.Execute((ProductionLineFeature)input);
 } while (input != 0);
 
+//Close connections
 client.Disconnect();
+await deviceClient.CloseAsync();
+await registryManager.CloseAsync();

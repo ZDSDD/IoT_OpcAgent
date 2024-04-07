f26255fd3749fac831bb63b1fc5ede43f19306bd
ZDSDD <106777224+ZDSDD@users.noreply.github.com>
2024-04-07 12:47:19 +0200

    Add device error enum flags

---
+++ OpcAgent.Lib/Enums/DeviceError.cs
@@ -0,0 +1,11 @@
+﻿namespace OpcAgent.Lib.Enums;
+
+[Flags]
+public enum DeviceError
+{
+    None = 0b_0000,
+    EmergencyStop = 0b_0001,
+    PowerFailure = 0b_0010,
+    SensorFailure = 0b_0100,
+    Unknown = 0b_1000
+}

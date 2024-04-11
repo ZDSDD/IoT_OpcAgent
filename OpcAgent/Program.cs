673c6933248ed09c2c1f6094f6a3d151fae610f2
ZDSDD <106777224+ZDSDD@users.noreply.github.com>
2024-04-12 00:37:47 +0200

    Handle exception and disable IoTHubManager for now

--- OpcAgent/Program.cs
+++ OpcAgent/Program.cs
@@ -1,14 +1,10 @@
-﻿using Microsoft.Azure.Devices;
-using Microsoft.Azure.Devices.Client;
+﻿using Microsoft.Azure.Devices.Client;
 using Microsoft.Extensions.Configuration;
 using Opc.Ua;
 using Opc.UaFx.Client;
-using OpcAgent;
 using OpcAgent.Enums.Feature;
-using OpcAgent.Lib;
 using OpcAgent.Lib.Device;
 using OpcAgent.Lib.Managers;
-using OpcAgent.Lib.Managers.Mediator;
 using OpcAgent.Lib.Selector.Implementation;
 using TransportType = Microsoft.Azure.Devices.Client.TransportType;
 
@@ -28,35 +24,44 @@ var device = new VirtualDevice(deviceClient);
 //Initialize OPCClient
 string serverAddress = config.GetConnectionString("serverAddress");
 using var client = new OpcClient(serverAddress);
-client.Connect();
 
-//Initialize Production Line Manager
-NodeId nodeId = new NodeId("ns=2;s=Device 1");
-ProductionLineManager productionLineManager = new ProductionLineManager(client, nodeId, device);
-
-//Initialize IoT Hub Manager
-string serviceConnectionString = config.GetConnectionString("IoTHub");
-using var serviceClient = ServiceClient.CreateFromConnectionString(serviceConnectionString);
-using var registryManager = RegistryManager.CreateFromConnectionString(serviceConnectionString);
-var iotHubManager = new IoTHubManager(serviceClient, registryManager);
-
-//Initialize Mediator between Managers
-new ManagersMediator(iotHubManager, productionLineManager);
-
-//Initialize Selectors
-ProductionLineFeatureSelector selector = new ProductionLineFeatureSelector(productionLineManager);
-
-//Main menu
-int input;
-do
+try
 {
-    selector.PrintMenu();
-    input = selector.ReadInput();
-    selector.Execute((ProductionLineFeature)input);
-} while (input != 0);
+    client.Connect();
 
-//Close connections
-client.Disconnect();
-await deviceClient.CloseAsync();
-await registryManager.CloseAsync();
-await serviceClient.CloseAsync();
+    //Initialize Production Line Manager
+    NodeId nodeId = new NodeId("ns=2;s=Device 1");
+    ProductionLineManager productionLineManager = new ProductionLineManager(client, nodeId, device);
+
+    //Initialize IoT Hub Manager
+    // string serviceConnectionString = config.GetConnectionString("IoTHub");
+    // using var serviceClient = ServiceClient.CreateFromConnectionString(serviceConnectionString);
+    // using var registryManager = RegistryManager.CreateFromConnectionString(serviceConnectionString);
+    // var iotHubManager = new IoTHubManager(serviceClient, registryManager);
+
+    //Initialize Selectors
+    ProductionLineFeatureSelector selector = new ProductionLineFeatureSelector(productionLineManager);
+
+    //Main menu
+    int input;
+    do
+    {
+        selector.PrintMenu();
+        input = selector.ReadInput();
+        selector.Execute((ProductionLineFeature)input);
+    } while (input != 0);
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.ToString());
+}
+finally
+{
+    //Close connections
+    client.Disconnect();
+    await deviceClient.CloseAsync();
+
+    //Uncomment for IoTHum manager
+    //await registryManager.CloseAsync();
+    //await serviceClient.CloseAsync();
+}
